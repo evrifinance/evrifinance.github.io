694603242a8133e2568de1a37615a0b47170a33c
evrifinance <139515530+evrifinance@users.noreply.github.com>
2024-01-23 20:14:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // JavaScript code goes here
-    console.log("Welcome to APP NAME - The Future of Finance");
-});
-
 // Test Firestore
 var db = firebase.firestore();
 
