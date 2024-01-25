23a1f44e69050e2494b37952fc39e72e9f7fe305
evrifinance <139515530+evrifinance@users.noreply.github.com>
2024-01-24 19:48:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
-const auth = getAuth();
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
 
+// TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyCbIz-jPc0GmQctyFJt1NMvB-gPp0Jl1Xk",
+	authDomain: "evri-finance.firebaseapp.com",
+	projectId: "evri-finance",
+	storageBucket: "evri-finance.appspot.com",
+	messagingSenderId: "144557667607",
+	appId: "1:144557667607:web:8f731961adfccf181d3352",
+	measurementId: "G-HR31XF4PDX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
 // Assuming you've already imported getAuth in your HTML
 
 document.addEventListener('DOMContentLoaded', (event) => {
