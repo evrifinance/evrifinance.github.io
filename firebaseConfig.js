e7868cabe2fda5e4b0d2fb65eb10162b1c6eebc4
evrifinance <139515530+evrifinance@users.noreply.github.com>
2024-06-13 19:12:19 -0500

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPuzqqUiNi2ChlVJf6BRUo5VnLb8-LnY4",
+    authDomain: "evrifinanceweb.firebaseapp.com",
+    projectId: "evrifinanceweb",
+    storageBucket: "evrifinanceweb.appspot.com",
+    messagingSenderId: "225994343734",
+    appId: "1:225994343734:web:f2c0c0de2d6999b8e21f97",
+    measurementId: "G-8X0YKJTX1Z"
+  };
