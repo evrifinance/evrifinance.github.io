900594e71dd67af61e7cf3431bad77b98be3f70f
evrifinance <139515530+evrifinance@users.noreply.github.com>
2024-06-13 19:20:27 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,3 +1,4 @@
+// Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCPuzqqUiNi2ChlVJf6BRUo5VnLb8-LnY4",
     authDomain: "evrifinanceweb.firebaseapp.com",
@@ -7,3 +8,6 @@ const firebaseConfig = {
     appId: "1:225994343734:web:f2c0c0de2d6999b8e21f97",
     measurementId: "G-8X0YKJTX1Z"
   };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
